e60ac1abe2b53afaeea704329e14ab171d6ef567
xsalvatore <xavier.salvatore.w@gmail.com>
2020-04-21 11:36:26 -0400

    section 8 - step 41 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // imports the packages
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // imports redux
@@ -13,25 +13,38 @@ import Alert from './components/layout/Alert';
 import { Login } from './components/auth/Login';
 import Register from './components/auth/Register';
 
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 // imports the css
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Alert />
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;

d3f2e5af27ede3299c9fa092f0e1e2a5fdfcb2d5
xsalvatore <xavier.salvatore.w@gmail.com>
2020-04-24 11:22:56 -0400

    section 9 - step 50 completed

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // imports our redux auth action to load a user
@@ -60,6 +62,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>

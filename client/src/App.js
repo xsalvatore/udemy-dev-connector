4fd005744855a15a16ab5e829000abe723926d41
xsalvatore <xavier.salvatore.w@gmail.com>
2020-04-29 11:11:46 -0400

    section 11 - step 61 completed

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // imports our redux auth action to load a user
@@ -77,6 +78,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,6 +5,7 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
+import post from './post';
 
 // exports the combined reducers
-export default combineReducers({ alert, auth, profile });
+export default combineReducers({ alert, auth, profile, post });

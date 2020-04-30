b49b646258a1bb3314357647b39a870a9b822976
xsalvatore <xavier.salvatore.w@gmail.com>
2020-04-30 14:34:18 -0400

    section 11 completed

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -155,7 +155,7 @@ export const addComment = (postId, formData) => async (dispatch) => {
 // deletes a comments
 export const deleteComment = (postId, commentId) => async (dispatch) => {
   try {
-    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
